5cb534213ce32ce8ed42bc249adfeb0598369dca
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2015-03-18 01:49:12 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'PS6USzK8jnN9GItyl6jprPgcF'
-  , consumer_secret: 'CEWZ8WqKPexsXjRagIf4FjcZ9AluJxuBzIzu04WwhfGEsCQNGZ'
-  , access_token: '16229305-8MabtSMHDQJuccKYgPPBDcVx0IMuTy7Yl82psVhXp'
-  , access_token_secret: 'EASzzOVyxaWUdyRz7jvIgafyRy5gucnLRj30vhnnOrtiD'
+    consumer_key: '...'
+  , consumer_secret: '...'
+  , access_token: '...'
+  , access_token_secret: '...'
 }
